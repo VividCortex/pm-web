723e5531f753bc3e83b4400a6a23c2c3f4da50c9
Devon VC_LAST_NAME <devon@vividcortex.com>
2014-02-28 19:28:21 -0500

    Added 'attrs: Object' to 'procs'. It is randomized for every port, and every execution, it is not randomized within each procs (It will be, but this will require modifying pm.go, and I have leave. But this will be be done Monday

--- server.go
+++ server.go
@@ -24,7 +24,7 @@ var mutex sync.Mutex
 
 var wg sync.WaitGroup
 
-func SomeProcess() {
+func SomeProcess(attributes *map[string]interface{}) {
 	wg.Add(1)
 	go func() {
 		mutex.Lock()
@@ -39,7 +39,7 @@ func SomeProcess() {
 			wg.Done()
 		}()
 
-		pm.Start(id, nil, nil)
+		pm.Start(id, nil, attributes)
 		defer pm.Done(id)
 
 		for _, status := range statuses {
@@ -47,22 +47,63 @@ func SomeProcess() {
 			pm.Status(id, status)
 		}
 
-		SomeProcess()
+		SomeProcess(attributes)
 	}()
 
 }
 
+// --- Not Inclusive of Maximum --- 
+func randInt(min int , max int) int {
+        rand.Seed( time.Now().UTC().UnixNano())
+        return min + rand.Intn(max-min)
+}
+
 func main() {
+	// --- Initialization
+	mapPack:= make(map[string]interface{})
+	taste := [] string{
+		"salty", 
+		"sweet", 
+		"sour",
+	}
+	color := [] string{
+		"red", 
+		"green", 
+		"blue",
+	}
+	temperature := [] string{
+		"hot", 
+		"medium", 
+		"cold",
+	}
+
+	// --- Randomize the Attributes
+	fmt.Println("RANDOMMMM")
+	numAttrs := randInt(1, 4)
+	for i:=0; i<numAttrs; i++ {
+		d1 := randInt(1,4)
+		d2 := randInt(0,3)
+		switch d1 {
+			case 1: mapPack["Taste"]=taste[d2]
+			case 2: mapPack["Color"]=color[d2]
+			case 3: mapPack["Temperature"]=temperature[d2]
+		}
+	}
+
+	// --- Command Line Parsing ---
 	port := flag.String("port", ":8081", "port string (ex. :8081)")
 	flag.Parse()
 
 	go pm.ListenAndServe(*port)
 
+	// --- Command Line Checking ---
 	fmt.Printf("Listening on localhost%s\n", *port)
 
 	for i := 0; i < 20; i++ {
-		SomeProcess()
+		fmt.Printf("Process %d\n", (i+1))
+		SomeProcess(&mapPack)
 	}
 
+	// --- ??? ---
 	wg.Wait()
 }
