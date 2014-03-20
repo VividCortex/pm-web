10a945548355ca31ae388b0a10cf3be44117a78d
Devon-Peroutky <devonperoutky@vividcortex.com>
2014-03-19 17:11:41 -0700

    Back to normal
    I know this was weird, but I had my reasons. Some of them were even valid.

--- app/js/proc-detail-controller.js
+++ app/js/proc-detail-controller.js
@@ -13,18 +13,12 @@ pmWebControllers.controller("ProcDetailCtrl", function($scope, $routeParams, $ht
   }
 
   var getHistory = function() {
-    console.log("Result: " + result);
     $http.get('http://'+$routeParams.host+'/procs/'+$routeParams.procId+'/history').then(function(result){
       var serverTime = result.data.serverTime;
       $scope.history = []
       for(var i in result.data.history) {
-        // --- Old ---
-        /*
         var millisecs = Date.parse(serverTime)-Date.parse(result.data.history[i].ts);
-        var cumulativeTime = toHHMMSS(millisecs/1000);*/
-        
-        // --- New ---
-        var cumulativeTime = toHHMMSS(i.Ts);
+        var cumulativeTime = toHHMMSS(millisecs/1000);
         $scope.history.push({cumulativeTime: cumulativeTime,
                              status:result.data.history[i].status})
       }
